12d2dc96c11a97c29a5175a395a3f2072432afab
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2023-02-02 17:30:30 +0200

    refactoring and add sentry

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const { Client, GatewayIntentBits, Partials, ActionRowBuilder, Events, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus, entersState } = require('@discordjs/voice');
+// import * as Sentry from "@sentry/browser";
+// import { BrowserTracing } from "@sentry/tracing";
+
+const Sentry = require("@sentry/browser")
+const { BrowserTracing } = require("@sentry/tracing")
+
+const { Client, GatewayIntentBits, Partials, ActionRowBuilder, Events, StringSelectMenuBuilder } = require('discord.js');
 require('dotenv').config();
 const { Station } = require("./structure/Station");
-const { Stations } = require('./utils/stations');
+const { createAudioResource } = require('@discordjs/voice');
+const { play, playWithList, stop, pause, unpause, getCategoriesButtons } = require("./utils/utils");
+
+
+Sentry.init({
+  dsn: process.env.DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates], partials: [Partials.Channel] });
-const QURAN_AUDIO_SOURCE = "https://stream.radiojar.com/8s5u5tpdtwzuv"
 
-const players = new Map();
+const servers = new Map();
 
 
 client.on('ready', () => {
@@ -44,25 +56,25 @@ client.on(Events.MessageCreate, async (message) => {
   }
 
   if (command === "list") {
-    await list(message)
+    await playWithList(message)
   }
 
   if (command === "play") {
-    await play(message, QURAN_AUDIO_SOURCE)
+    await play(servers, message)
   }
 
   if (command === "stop") {
-    await stop(message)
+    await stop(servers, message)
   }
 
 
   if (command === "pause") {
-    await pause(message)
+    await pause(servers, message)
   }
 
 
   if (command === "unpause") {
-    await unpause(message)
+    await unpause(servers, message)
   }
 
 
@@ -100,9 +112,9 @@ client.on(Events.InteractionCreate, async (message) => {
     let guild_id = message.guild.id;
     let station = Station.getInstance().getStation(selected);
 
-    let conn = players.get(guild_id);
+    let conn = servers.get(guild_id);
     if (!conn) {
-      await play(message, station.radio_url)
+      await play(servers, message, station.radio_url)
       return
     }
     const resource =
@@ -118,106 +130,7 @@ client.on(Events.InteractionCreate, async (message) => {
 });
 
 
-function getCategoriesButtons(selectedCategory = "قراء") {
 
-  let categories = Station.getInstance().getCategories();
-  let buttons = [];
-  let btns = []
-  let i = 1;
-  for (category of categories) {
-    if (!btns[i % 3]) {
-      btns[i % 3] = []
-    }
-    btns[i % 3].push(new ButtonBuilder().setCustomId(category).setLabel(category).setStyle(category == selectedCategory ? ButtonStyle.Success : ButtonStyle.Secondary))
-    i++;
-  }
-
-  btns.forEach(btn => {
-    buttons.push(new ActionRowBuilder().addComponents(...btn))
-  })
-  return buttons;
-}
-
-async function list(message) {
-  let mappedStations = Station.getInstance().getMappedStations();
-
-  const selectMenu = new ActionRowBuilder()
-    .addComponents(
-      new StringSelectMenuBuilder()
-        .setCustomId('select')
-        .setPlaceholder('Nothing selected')
-        .addOptions(
-          mappedStations.slice(0, 20)
-        ),
-
-    );
-
-  let buttons = getCategoriesButtons()
-
-  await message.reply({ content: 'Stations!', components: [selectMenu, ...buttons] });
-}
-
-async function stop(message) {
-  let connection = players.get(message.guild.id).connection
-  connection.destroy();
-  players.delete(message.guild.id)
-  message.reply("stopped Quran Station")
-}
-
-async function pause(message) {
-  let player = players.get(message.guild.id).player
-  player.pause()
-  message.reply("paused Quran Station")
-}
-
-async function unpause(message) {
-  let player = players.get(message.guild.id).player
-  player.unpause()
-  message.reply("Unpause Quran Station")
-}
-
-
-
-async function play(message, url) {
-  const voiceChannel = message.member.voice.channel;
-
-  if (!voiceChannel)
-    return message.reply(
-      "You need to be in a voice channel to play!"
-    );
-
-
-  try {
-
-    const connection = joinVoiceChannel({
-      channelId: voiceChannel.id,
-      guildId: message.guild.id,
-      adapterCreator: message.guild.voiceAdapterCreator,
-    });
-
-
-    const resource =
-      createAudioResource(url, {
-        inlineVolume: true
-      })
-
-    const player = createAudioPlayer();
-    connection.subscribe(player)
-    players.set(message.guild.id, {
-      connection: connection,
-      player: player,
-      resource: resource
-    })
-
-    player.play(resource)
-    message.reply("Playing Quran Station")
-
-  } catch (err) {
-    console.log(err);
-    return message.channel.send(err);
-  }
-
-}
 
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
---
+++ utils/utils.js
@@ -0,0 +1,108 @@
+const QURAN_AUDIO_SOURCE = "https://stream.radiojar.com/8s5u5tpdtwzuv"
+const { ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const { Station } = require("../structure/Station");
+
+function getCategoriesButtons(selectedCategory = "قراء") {
+  let categories = Station.getInstance().getCategories();
+  let buttons = [];
+  let btns = []
+  let i = 0;
+  for (category of categories) {
+    if (!btns[i]) {
+      btns[i] = []
+    }
+    btns[i].push(new ButtonBuilder().setCustomId(category).setLabel(category).setStyle(category == selectedCategory ? ButtonStyle.Success : ButtonStyle.Secondary))
+    if (btns[i].length == 5) i++;
+  }
+  btns.forEach(btn => {
+    buttons.push(new ActionRowBuilder().addComponents(...btn))
+  })
+  return buttons;
+}
+
+
+
+async function playWithList(message) {
+  let mappedStations = Station.getInstance().getMappedStations();
+
+  const selectMenu = new ActionRowBuilder()
+    .addComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId('select')
+        .setPlaceholder('Nothing selected')
+        .addOptions(
+          mappedStations.slice(0, 20)
+        ),
+
+    );
+
+  let buttons = getCategoriesButtons()
+
+  await message.reply({ content: 'Stations!', components: [selectMenu, ...buttons] });
+}
+
+async function stop(servers, message) {
+  let connection = servers.get(message.guild.id).connection
+  connection.destroy();
+  servers.delete(message.guild.id)
+  message.reply("stopped Quran Station")
+}
+
+async function pause(servers, message) {
+  let player = servers.get(message.guild.id).player
+  player.pause()
+  message.reply("paused Quran Station")
+}
+
+async function unpause(servers, message) {
+  let player = servers.get(message.guild.id).player
+  player.unpause()
+  message.reply("Unpause Quran Station")
+}
+
+
+
+async function play(servers, message, url = QURAN_AUDIO_SOURCE) {
+  const voiceChannel = message.member.voice.channel;
+
+  if (!voiceChannel)
+    return message.reply(
+      "You need to be in a voice channel to play!"
+    );
+
+
+  try {
+
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    });
+
+
+    const resource =
+      createAudioResource(url, {
+        inlineVolume: true
+      })
+
+    const player = createAudioPlayer();
+    connection.subscribe(player)
+    servers.set(message.guild.id, {
+      connection: connection,
+      player: player,
+      resource: resource
+    })
+
+    player.play(resource)
+    message.reply("Playing Quran Station")
+
+  } catch (err) {
+    console.log(err);
+    return message.channel.send(err);
+  }
+
+}
+
+module.exports = { playWithList, play, stop, pause, unpause, getCategoriesButtons }
+
